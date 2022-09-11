7d6eae034bce8062088ccc6fb3d93568d452bfc5
MYefimenko <marinka0410@gmail.com>
2022-09-11 21:18:27 +0200

    addedJS

---
+++ js/script.js
@@ -0,0 +1,57 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let deadLine = new Date(2023, 0, 1, 00, 00);
+
+    function getTimeRemainig (endtime) {
+        let t = Date.parse(endtime) - new Date(),
+            days = Math.floor(t/1000/60/60/24),
+            hours = Math.floor((t/1000/60/60) % 24),
+            minutes = Math.floor((t/1000/60) % 60),
+            seconds = Math.floor((t/1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+        };
+    }
+
+    function getZero(num){
+        if(num >= 0 && num < 10){
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock (selector, endtime){
+        let timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+            
+            updateClock();
+
+            function updateClock(){
+                let t = getTimeRemainig (endtime);
+                days.innerHTML = getZero(t.days);
+                hours.innerHTML = getZero(t.hours);
+                minutes.innerHTML = getZero(t.minutes);
+                seconds.innerHTML = getZero(t.seconds);
+
+                if(t.total <= 0) {
+                    clearInterval(timeInterval);
+                }
+            }
+    }
+
+    setClock('.timer', deadLine);
+
+    let deadLine1 = '2023-01-01';
+    console.log(Date.parse(deadLine1));
+    console.log(Date.parse(deadLine1) - Date.parse(new Date()));
+
+})
