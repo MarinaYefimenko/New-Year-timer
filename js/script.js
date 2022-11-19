a185beed2b5d09f6a2ddcb24fafdb9cd8746a8f5
MYefimenko <marinka0410@gmail.com>
2022-11-19 18:22:11 +0100

    2023

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
     let deadLine = new Date(2023, 0, 1, 00, 00);
 
-    function getTimeRemainig (endtime) {
+    function getTimeRemainig(endtime) {
         let t = Date.parse(endtime) - new Date(),
-            days = Math.floor(t/1000/60/60/24),
-            hours = Math.floor((t/1000/60/60) % 24),
-            minutes = Math.floor((t/1000/60) % 60),
-            seconds = Math.floor((t/1000) % 60);
+            days = Math.floor(t / 1000 / 60 / 60 / 24),
+            hours = Math.floor((t / 1000 / 60 / 60) % 24),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
 
         return {
             'total': t,
@@ -17,41 +17,36 @@ window.addEventListener('DOMContentLoaded', () => {
         };
     }
 
-    function getZero(num){
-        if(num >= 0 && num < 10){
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
             return `0${num}`;
         } else {
             return num;
         }
     }
 
-    function setClock (selector, endtime){
+    function setClock(selector, endtime) {
         let timer = document.querySelector(selector),
             days = timer.querySelector('#days'),
             hours = timer.querySelector('#hours'),
             minutes = timer.querySelector('#minutes'),
             seconds = timer.querySelector('#seconds'),
             timeInterval = setInterval(updateClock, 1000);
-            
-            updateClock();
 
-            function updateClock(){
-                let t = getTimeRemainig (endtime);
-                days.innerHTML = getZero(t.days);
-                hours.innerHTML = getZero(t.hours);
-                minutes.innerHTML = getZero(t.minutes);
-                seconds.innerHTML = getZero(t.seconds);
+        updateClock();
 
-                if(t.total <= 0) {
-                    clearInterval(timeInterval);
-                }
+        function updateClock() {
+            let t = getTimeRemainig(endtime);
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
             }
+        }
     }
 
     setClock('.timer', deadLine);
-
-    let deadLine1 = '2023-01-01';
-    console.log(Date.parse(deadLine1));
-    console.log(Date.parse(deadLine1) - Date.parse(new Date()));
-
 })
